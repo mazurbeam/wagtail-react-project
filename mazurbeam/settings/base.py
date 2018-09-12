490e394bf5e0fa577273cc95fb54865e7dd2367a
mazurbeam <mazurbeam@gmail.com>
2018-09-12 16:24:12 -0700

    whitenosie middleware

--- mazurbeam/settings/base.py
+++ mazurbeam/settings/base.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'wagtail.core.middleware.SiteMiddleware',
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
 ]

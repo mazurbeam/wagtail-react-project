eb2eb9ff625cefce1c6c9b3ff0c95e66cae167a8
mazurbeam <mazurbeam@gmail.com>
2018-09-16 10:47:09 -0700

    updated requirements

--- mazurbeam/wsgi.py
+++ mazurbeam/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mazurbeam.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mazurbeam.settings.dev")
 
 application = get_wsgi_application()

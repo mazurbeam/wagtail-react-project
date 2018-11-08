35c6483fd227981de1ededd8c1fbc69e8ced8fd7
mazurbeam <mazurbeam@gmail.com>
2018-11-07 16:46:04 -0800

    particles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import { ConnectedRouter } from 'connected-react-router'
 
-// import ParticlesWrapper from './components/ParticlesWrapper'
+import ParticlesWrapper from './components/ParticlesWrapper'
 import Routes from './routes'
 import theme from './theme/styles/theme'
 import './static/css/uikit.css'
@@ -10,6 +10,7 @@ import './static/css/uikit.css'
 const App = ({ history }) => {
   return (
     <div>
+      <ParticlesWrapper />
       <ThemeProvider theme={theme}>
         <ConnectedRouter history={history}>
           <Routes />

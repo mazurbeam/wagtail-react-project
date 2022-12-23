9eeb382a34e132032aeaef34789b85fff4501317
Jessie <70440141+jessiebelle@users.noreply.github.com>
2022-12-23 22:07:38 +0000

    working wagtail

---
+++ shownotes/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-wb=1_g0uaf!+l_^9&f4#aoorocrd5_gcm&y6*ni4za9iw4n7$9"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass

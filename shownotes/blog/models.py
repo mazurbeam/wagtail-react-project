11a23ff719f865fd56392fd9aa7c37e405296b76
Jessie <70440141+jessiebelle@users.noreply.github.com>
2022-12-24 20:14:27 +0000

    adding categories to blog

--- shownotes/blog/models.py
+++ shownotes/blog/models.py
@@ -1,12 +1,12 @@
+from django import forms
+
 from django.db import models
-
-from modelcluster.fields import ParentalKey
 from modelcluster.contrib.taggit import ClusterTaggableManager
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
 from taggit.models import TaggedItemBase
-
-from wagtail.models import Page, Orderable
-from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel
+from wagtail.fields import RichTextField
+from wagtail.models import Orderable, Page
 from wagtail.search import index
 from wagtail.snippets.models import register_snippet
 
